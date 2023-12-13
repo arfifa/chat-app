46195122197773eda2a0d0723b3b935bbb3cd7db
arfifa <arfifarahman509@gmail.com>
2023-12-13 18:30:29 +0700

    add section header footer message box in dasboard page

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { Box, Stack, useTheme } from "@mui/material";
+
+// custom component
 import Chats from "./Chats";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
+  const theme = useTheme();
+
   return (
-    <>
+    <Stack direction={"row"} sx={{ width: "100%" }}>
       {/* Chats  */}
       <Chats />
-    </>
+
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#fff"
+              : theme.palette.background.default,
+        }}
+      >
+        {/* Conversation  */}
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
