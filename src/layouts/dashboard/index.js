b90297cf4a4e37bd1d6b3cd863566acfcd85b723
arfifa <arfifarahman509@gmail.com>
2023-12-13 12:38:55 +0700

    Add Section ChatList

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -75,41 +75,78 @@ const DashboardLayout = () => {
 
   return (
     <>
-      <Box
-        p={2}
-        sx={{
-          backgroundColor: theme.palette.background.paper,
-          boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
-          height: "100vh",
-          width: 100,
-        }}
-      >
-        <Stack
-          direction={"column"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
-          sx={{ height: "100%" }}
-          spacing={3}
+      <Stack direction="row">
+        <Box
+          p={2}
+          sx={{
+            backgroundColor: theme.palette.background.paper,
+            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+            height: "100vh",
+            width: 100,
+          }}
         >
-          <Stack spacing={4} alignItems={"center"}>
-            <Box
-              sx={{
-                backgroundColor: theme.palette.primary.main,
-                height: 64,
-                width: 64,
-                borderRadius: 1.5,
-              }}
-            >
-              <img src={Logo} alt="Chat App Logo" />
-            </Box>
-            <Stack
-              spacing={3}
-              direction={"column"}
-              alignItems={"center"}
-              sx={{ width: "max-content" }}
-            >
-              {Nav_Buttons.map((el) =>
-                el.index === selected ? (
+          <Stack
+            direction={"column"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            sx={{ height: "100%" }}
+            spacing={3}
+          >
+            <Stack spacing={4} alignItems={"center"}>
+              <Box
+                sx={{
+                  backgroundColor: theme.palette.primary.main,
+                  height: 64,
+                  width: 64,
+                  borderRadius: 1.5,
+                }}
+              >
+                <img src={Logo} alt="Chat App Logo" />
+              </Box>
+              <Stack
+                spacing={3}
+                direction={"column"}
+                alignItems={"center"}
+                sx={{ width: "max-content" }}
+              >
+                {Nav_Buttons.map((el) =>
+                  el.index === selected ? (
+                    <Box
+                      sx={{
+                        backgroundColor: theme.palette.primary.main,
+                        borderRadius: 1.5,
+                      }}
+                    >
+                      <IconButton
+                        sx={{
+                          width: "max-content",
+                          color: "#fff",
+                        }}
+                        key={el.index}
+                      >
+                        {el.icon}
+                      </IconButton>
+                    </Box>
+                  ) : (
+                    <IconButton
+                      sx={{
+                        width: "max-content",
+                        color:
+                          theme.palette.mode === "light"
+                            ? "#000"
+                            : theme.palette.text.primary,
+                      }}
+                      key={el.index}
+                      onClick={() => {
+                        setSelected(el.index);
+                      }}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  )
+                )}
+                <Divider sx={{ width: "48px" }} />
+                {selected === 3 ? (
                   <Box
                     sx={{
                       backgroundColor: theme.palette.primary.main,
@@ -121,9 +158,8 @@ const DashboardLayout = () => {
                         width: "max-content",
                         color: "#fff",
                       }}
-                      key={el.index}
                     >
-                      {el.icon}
+                      <Gear />
                     </IconButton>
                   </Box>
                 ) : (
@@ -135,57 +171,23 @@ const DashboardLayout = () => {
                           ? "#000"
                           : theme.palette.text.primary,
                     }}
-                    key={el.index}
                     onClick={() => {
-                      setSelected(el.index);
-                    }}
-                  >
-                    {el.icon}
-                  </IconButton>
-                )
-              )}
-              <Divider sx={{ width: "48px" }} />
-              {selected === 3 ? (
-                <Box
-                  sx={{
-                    backgroundColor: theme.palette.primary.main,
-                    borderRadius: 1.5,
-                  }}
-                >
-                  <IconButton
-                    sx={{
-                      width: "max-content",
-                      color: "#fff",
+                      setSelected(3);
                     }}
                   >
                     <Gear />
                   </IconButton>
-                </Box>
-              ) : (
-                <IconButton
-                  sx={{
-                    width: "max-content",
-                    color:
-                      theme.palette.mode === "light"
-                        ? "#000"
-                        : theme.palette.text.primary,
-                  }}
-                  onClick={() => {
-                    setSelected(3);
-                  }}
-                >
-                  <Gear />
-                </IconButton>
-              )}
+                )}
+              </Stack>
+            </Stack>
+            <Stack spacing={4}>
+              <AntSwitch onChange={onToggleMode} defaultChecked />
+              <Avatar src={faker.image.avatar()} />
             </Stack>
           </Stack>
-          <Stack spacing={4}>
-            <AntSwitch onChange={onToggleMode} defaultChecked />
-            <Avatar src={faker.image.avatar()} />
-          </Stack>
-        </Stack>
-      </Box>
-      <Outlet />
+        </Box>
+        <Outlet />
+      </Stack>
     </>
   );
 };
